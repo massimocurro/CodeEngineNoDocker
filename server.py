258899990365610bf84f8d5d4640e756393bda09
Massimo Curro <m.curro@gruppostratos.com>
2023-12-20 11:39:33 +0100

    FIX

--- server.py
+++ server.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return pip._vendor.requests.get("https://function-76.1at6rgz00yjr.eu-de.codeengine.appdomain.cloud")
+    pip._vendor.requests.get("https://function-76.1at6rgz00yjr.eu-de.codeengine.appdomain.cloud")
+    return "OK"
 
 
 if __name__ == '__main__':
